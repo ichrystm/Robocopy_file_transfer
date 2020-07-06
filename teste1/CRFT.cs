59eaa87705ccd0a7fdaae4529e38b257577044c9
Chrysler de Oliveira <57509231+ichrystm@users.noreply.github.com>
2020-07-06 18:19:21 -0300

    Update CRFT

--- teste1/CRFT.cs
+++ teste1/CRFT.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -66,45 +66,39 @@ namespace teste1
                 Process My_Process = new Process();
                 ProcessStartInfo My_Process_Info = new ProcessStartInfo();
 
-               
 
-                My_Process_Info.FileName = "cmd.exe";
+                My_Process_Info.FileName = "cmd.exe"; 
 
                 
 
                 if (caminhoOrigem != "" && caminhoDestino != "")
                 {
-                    
+
+                    var splitOrigem = caminhoOrigem;
+                    string[] strOrigem = splitOrigem.Split('\\');
+                    var pastaDestino = strOrigem[strOrigem.Length - 1];
 
                     My_Process_Info.WorkingDirectory = @"c:\temp";
-                    My_Process_Info.Arguments = "/k robocopy \"" + caminhoOrigem + "\"" + " " + "\"" + caminhoDestino + "\"" + " /e /z /eta /v /bytes" + " /log:" + '\u0022' + "c:\"temp\"logpasta.txt" + '\u0022' + " /tee";
-                    My_Process_Info.CreateNoWindow = false;
-                    My_Process_Info.UseShellExecute = false;
-                    My_Process_Info.RedirectStandardOutput = false;
-                    My_Process_Info.RedirectStandardError = false;
-                    My_Process.EnableRaisingEvents = true;
+                    My_Process_Info.Arguments = "/c robocopy \"" + caminhoOrigem + "\"" + " " + "\"" + caminhoDestino + "\\" + pastaDestino + "\"" + " /e /z /eta /v /bytes";
+                    My_Process_Info.CreateNoWindow = false; 
+                    My_Process_Info.UseShellExecute = false; 
+                    My_Process_Info.RedirectStandardOutput = false; 
+                    My_Process_Info.RedirectStandardError = false; 
+                    My_Process.EnableRaisingEvents = true; 
                     My_Process.StartInfo = My_Process_Info;
                     My_Process.Start();
                     My_Process.WaitForExit();
 
-                }
-                    //if(caminhoOrigem != "" && caminhoDestino != "")
-                    DialogResult dialogResult = MessageBox.Show("Deseja verificar os detalhes da trânsferência?", "Concluído", MessageBoxButtons.YesNo);
-                    if (dialogResult == DialogResult.Yes)
-                    {
-                    System.Diagnostics.Process.Start(@"c:\temp\templog.txt");
-                    TXT_Destino.Clear();
-                    TXT_Origem.Clear();
-                }
-                else
-                {
-                    TXT_Origem.Clear();
-                    TXT_Destino.Clear();
-                }
+                    MessageBox.Show("Transferência concluída.", "Concluído", MessageBoxButtons.OK);
 
+                }
+                   
+                   
+                    TXT_Origem.Clear();
+                    TXT_Destino.Clear();    
                 
 
-            }else if (radioButton2.Checked)
+            } else if (radioButton2.Checked)
             {
                 var caminhoOrigem = fileorigem.FileName;
                 var caminhoDestino = FB_Destino.SelectedPath;
@@ -112,8 +106,9 @@ namespace teste1
                 Process My_Process = new Process();
                 ProcessStartInfo My_Process_Info = new ProcessStartInfo();
 
+                
 
-                My_Process_Info.FileName = "cmd.exe";
+                My_Process_Info.FileName = "cmd.exe"; 
 
 
 
@@ -121,15 +116,15 @@ namespace teste1
                 {
                     
                     My_Process_Info.WorkingDirectory = @"c:\temp";
-                    My_Process_Info.Arguments = "/c copy \"" + caminhoOrigem + "\"" + " " + "\"" + caminhoDestino + "\"";
-                    My_Process_Info.CreateNoWindow = false;
-                    My_Process_Info.UseShellExecute = false;
-                    My_Process_Info.RedirectStandardOutput = false;
-                    My_Process_Info.RedirectStandardError = false;
-                    My_Process.EnableRaisingEvents = true;
+                    My_Process_Info.Arguments = "/c copy \"" + caminhoOrigem + "\"" + " " + "\"" + caminhoDestino + "\""; 
+                    My_Process_Info.CreateNoWindow = false; 
+                    My_Process_Info.UseShellExecute = false; 
+                    My_Process_Info.RedirectStandardOutput = false; 
+                    My_Process_Info.RedirectStandardError = false; 
+                    My_Process.EnableRaisingEvents = true; 
                     My_Process.StartInfo = My_Process_Info;
-                    My_Process.Start();
-                    My_Process.WaitForExit(); 
+                    My_Process.Start(); 
+                    My_Process.WaitForExit();
 
                     MessageBox.Show("Transferência concluída.", "Concluído", MessageBoxButtons.OK);
 
@@ -138,8 +133,6 @@ namespace teste1
                     caminhoOrigem = "";
                     caminhoDestino = "";
                 }
-                
-                //DialogResult dialogResult = MessageBox.Show("Transferência concluida!", "Concluído", MessageBoxButtons.OK);
 
                     
                
@@ -160,4 +153,3 @@ namespace teste1
 
 
     }
-
